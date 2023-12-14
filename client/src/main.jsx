d02698c925ebdcbad438ba4998080200eb539380
akshaydobariya <avpatel7775@gmail.com>
2023-12-14 10:42:36 +0530

    auth0-1

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,13 +7,13 @@ import { Auth0Provider } from "@auth0/auth0-react";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Auth0Provider
-     domain="dev-03ifqltxbr6nn0hn.us.auth0.com"
-     clientId="RXlGXkr49Ev5MHpvAC6vKkZ4bVn11iwl"
-     authorizationParams={{
-      redirect_uri: "https://full-stack-real-estate-youtube-sooty.vercel.app"
-     }}
-     audience="http://localhost:8000"
-     scope="openid profile email"
+      domain="dev-kpcvdyah47mtfmjt.us.auth0.com"
+      clientId="houovPbYLzazMSX3eUH64HjG6pvoZagu"
+      authorizationParams={{
+        redirect_uri: "https://realestate-ruby-zeta.vercel.app/api",
+      }}
+      audience="http://localhost:8000"
+      scope="openid profile email"
     >
       <App />
     </Auth0Provider>
